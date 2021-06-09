bb492395242442309ad1f230468dadecb1a316d6
JimmyShi22 <417711026@qq.com>
2021-06-09 15:05:28 +0800

    Add get all alg types

--- src/main/java/org/luyu/protocol/algorithm/SignAlgManager.java
+++ src/main/java/org/luyu/protocol/algorithm/SignAlgManager.java
@@ -1,10 +1,20 @@
 package org.luyu.protocol.algorithm;
 
+import java.util.LinkedList;
+import java.util.List;
 import org.luyu.protocol.algorithm.ecdsa.secp256k1.EcdsaSecp256k1WithSHA256;
 import org.luyu.protocol.algorithm.ecdsa.secp256r1.EcdsaSecp256r1;
 import org.luyu.protocol.algorithm.sm2.SM2WithSM3;
 
 public class SignAlgManager {
+    public static List<String> getAllAlgTypes() {
+        List<String> types = new LinkedList<>();
+        types.add(SM2WithSM3.TYPE);
+        types.add(EcdsaSecp256k1WithSHA256.TYPE);
+        types.add(EcdsaSecp256r1.TYPE);
+        return types;
+    }
+
     public static SignatureAlgorithm getAlgorithm(String type) {
         switch (type) {
             case SM2WithSM3.TYPE:
