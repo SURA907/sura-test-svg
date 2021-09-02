eae0ee654e70002527e1cab412eb94d83a5d870b
JimmyShi22 <417711026@qq.com>
2021-09-02 12:20:49 +0800

    remove duplicate params of Events

--- src/main/java/link/luyu/protocol/network/Events.java
+++ src/main/java/link/luyu/protocol/network/Events.java
@@ -8,20 +8,18 @@ public interface Events {
     /**
      * Send transaction to another resource through router
      *
-     * @param account Luyu Account
      * @param tx The transaction to destination blockchain (remain 'key' and 'LuyuSign' empty)
      * @param callback
      */
-    void sendTransaction(Account account, Transaction tx, Driver.ReceiptCallback callback);
+    void sendTransaction(Transaction tx, Driver.ReceiptCallback callback);
 
     /**
      * Call to another resource through router
      *
-     * @param account Luyu Account
      * @param request The callRequest to destination blockchain
      * @param callback
      */
-    void call(Account account, CallRequest request, Driver.CallResponseCallback callback);
+    void call(CallRequest request, Driver.CallResponseCallback callback);
 
     interface KeyCallback {
         /** @param account chain account */
